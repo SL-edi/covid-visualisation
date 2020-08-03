0d3a24d7239a66a796df2c32dfc387fbca72b9b0
duncstin <duncan.austin@hotmail.co.uk>
2020-08-03 10:22:43 +0100

    add countries-table component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CountriesTableComponent } from './countries-table/countries-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountriesTableComponent
   ],
   imports: [
     BrowserModule
