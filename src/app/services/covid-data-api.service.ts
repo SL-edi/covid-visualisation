368b41725bcf1bc8292d5693f29f943670ce2d52
Adrian Borg <borgadrian@yahoo.co.uk>
2020-09-29 14:56:11 +0100

    :recycle: refactor covid-data-api to use historical apis too

--- src/app/services/covid-data-api.service.ts
+++ src/app/services/covid-data-api.service.ts
@@ -2,9 +2,13 @@ import { Injectable, Inject, InjectionToken } from '@angular/core';
 import { CovidDataPoint } from '../models/CovidDataPoint';
 
 import { Subject } from 'rxjs';
+import { CountryHistoricalDataPoint } from './externalApis/common-covid-19-api';
 
 export interface CovidDataApiSubService {
-  call(successCallback: (x: any) => void, errorCallback: (x?: any) => void): void;
+  call(
+    successCallback: (x: any) => void,
+    errorCallback: (x?: any) => void
+  ): void;
 }
 
 export const COVID_DATA_API_SUB_SERVICE_SUMMARY = new InjectionToken<
@@ -19,12 +23,20 @@ export const COVID_DATA_API_SUB_SERVICE_HISTORICAL = new InjectionToken<
   providedIn: 'root',
 })
 export class CovidDataApiService {
-  summaryDataObserver: Subject<CovidDataPoint[]> = new Subject<CovidDataPoint[]>();
+  summaryDataObserver: Subject<CovidDataPoint[]> = new Subject<
+    CovidDataPoint[]
+  >();
+  historicalDataObserver: Subject<CountryHistoricalDataPoint[]> = new Subject<
+    CountryHistoricalDataPoint[]
+  >();
+
   constructor(
     // Inspired by (https://stackoverflow.com/a/35916788)
     // Each api service must have its own provider registered in app.module for the InjectionToken
     @Inject(COVID_DATA_API_SUB_SERVICE_SUMMARY)
-    private apiServices: CovidDataApiSubService[]
+    private summaryApiServices: CovidDataApiSubService[],
+    @Inject(COVID_DATA_API_SUB_SERVICE_HISTORICAL)
+    private historicalApiServices: CovidDataApiSubService[]
   ) {
     this.getCovidSummaryData();
   }
@@ -34,13 +46,25 @@ export class CovidDataApiService {
    * If all lead to errors, prints an error message to the console
    */
   private getCovidSummaryData(): void {
-    const callFunction = this.apiServices.reduceRight<(e?: any) => void>(
-      (fallback: () => void, api: CovidDataApiSubService) => () => api.call(
-        result => this.summaryDataObserver.next(result),
-        fallback
-      ),
-      () => console.error('All api sub-services responded with errors')
+    this.callApis(this.summaryApiServices, this.summaryDataObserver);
+  }
+
+  getCovidHistoricalData(): void {
+    this.callApis(this.historicalApiServices, this.historicalDataObserver);
+  }
+
+  private callApis = (
+    apiServices: CovidDataApiSubService[],
+    subject: Subject<any>
+  ) => {
+    const callFunction = apiServices.reduceRight<(e?: any) => void>(
+      (fallback: () => void, api: CovidDataApiSubService) => 
+          () => api.call(
+            (result) => subject.next(result), 
+            fallback
+          ),
+          () => console.error('All api sub-services responded with errors')
     );
     callFunction();
-  }
+  };
 }
