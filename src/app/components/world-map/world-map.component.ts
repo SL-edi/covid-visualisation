53424fd1bcbbac2167807135b51cf374064e1c65
Tadeusz Janowski <tadjanow@gmail.com>
2020-09-29 17:58:34 +0100

    Added markers to the map

--- src/app/components/world-map/world-map.component.ts
+++ src/app/components/world-map/world-map.component.ts
@@ -1,8 +1,17 @@
 import { AfterViewInit, Component, Inject } from '@angular/core';
-import { map, Map, tileLayer } from 'leaflet';
-import { MapProvider } from 'src/app/services/map-provider.service';
+import { circle, map, Map, tileLayer } from 'leaflet';
+import { MapProvider, MAP_PROVIDER } from 'src/app/services/map-provider.service';
+import { CovidDataApiService } from 'src/app/services/covid-data-api.service';
+import { byCountryCode } from 'country-finder';
 
 import { MapLocation } from '../../models/MapLocation';
+import { CovidDataPoint } from 'src/app/models/CovidDataPoint';
+import { Color } from 'ag-grid-community';
+
+const INFECTED_COLOUR = 'red';
+const DEAD_COLOUR = 'black';
+const RECOVERED_COLOUR = 'green';
+const SCALE_FACTOR = 500; // Size of markers in m*(people)^(-1/2)
 
 @Component({
   selector: 'app-world-map',
@@ -20,11 +29,17 @@ export class WorldMapComponent implements AfterViewInit {
   private mapSelector = 'map';
 
   constructor(
-    @Inject('MapProvider') private mapProvider: MapProvider
+    @Inject(MAP_PROVIDER) private mapProvider: MapProvider,
+    private apiService: CovidDataApiService
   ) {}
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.apiService.summaryDataObserver.subscribe((data) => {
+      this.map.remove(); // Clear map before applying the update
+      this.initMap();
+      this.addData(data);
+    });
   }
 
   private initMap(): void {
@@ -41,5 +56,21 @@ export class WorldMapComponent implements AfterViewInit {
     );
 
     tiles.addTo(this.map);
+  }
+
+  private addData(data: CovidDataPoint[]): void {
+    data.forEach(({ location, confirmed, dead, recovered }: CovidDataPoint) => {
+      const { lat, long } = byCountryCode(location);
+
+      // Draws 3 circles for each data point - recovered, dead, infected
+      // Each AREA is proportional to the number of cases
+      const recoveredRadius = SCALE_FACTOR * Math.sqrt(recovered);
+      const deadRadius = SCALE_FACTOR * Math.sqrt(dead);
+      const infectedRadius = SCALE_FACTOR * Math.sqrt(confirmed - dead - recovered);
+
+      circle([lat, long], { radius: recoveredRadius, color: RECOVERED_COLOUR }).addTo(this.map);
+      circle([lat, long], { radius: deadRadius, color: DEAD_COLOUR }).addTo(this.map);
+      circle([lat, long], { radius: infectedRadius, color: INFECTED_COLOUR }).addTo(this.map);
+    });
   }
 }
